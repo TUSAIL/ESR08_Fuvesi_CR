a359ac9678b9ae2a3360d8d628ea86147ef584f1
gestoe <gero.stoeckl@gmx.net>
2023-05-10 12:00:46 +0200

    added conversion to fahrenheit

--- conversion.py
+++ conversion.py
@@ -1,3 +1,7 @@
 def celsius_to_kelvin(celsius):
     """Function to return Kelvin degrees from Celsius input"""
     return celsius + 273.15
+
+def celsius_to_fahrenheit(celsius):
+    """Function to return Kelvin degrees from Celsius input"""
+    return celsius *9/5 + 32
