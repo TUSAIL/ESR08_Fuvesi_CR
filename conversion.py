01a89a5b0fb1653312b21aca03ae00ea408edaed
Balazs Fuvesi <fuvesib@gmail.com>
2023-05-16 12:23:23 +0200

    Add docstring

--- conversion.py
+++ conversion.py
@@ -1,3 +1,34 @@
+import pandas as pd
+
+def x(a, print_columns=False):
+   b = pd.read_excel(a)
+   column_headers = list(b.columns.values)
+   if print_columns:
+       print("\n".join(column_headers))
+   return column_headers
+
 def celsius_to_kelvin(celsius):
-    """Function to return Kelvin degrees from Celsius input"""
+    """
+    Function to return Kelvin degrees from Celsius input
+    """
     return celsius + 273.15
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9./5 + 32
+
+def mean_temperature(data):
+    """
+    Get the mean temperature.
+    
+    Args:
+        data (pandas.DataFrame): A pandas dataframe with air temperature measurements.
+
+    Returns:
+        The mean air temperature (float)
+    """
+    temperatures = data['Air temerature (degC)']
+    return sum(temperatures)/len(temperatures)
+
